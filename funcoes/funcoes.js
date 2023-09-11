aa3bfb10c13a7726f972a9081e325d5fff65f032
Daniel Gimenes <100932065+projdanielgimenes@users.noreply.github.com>
2023-09-11 08:43:14 -0300

    Add files via upload

---
+++ funcoes/funcoes.js
@@ -0,0 +1,10 @@
+saldo = 1000
+function dadosacesso(){
+    numcartao = prompt("Insira o número do seu cartão: ")
+    senha = prompt("Insira a sua senha: ")
+    
+}
+function saque(valor){
+    saldo = (saldo - valor)
+    return saldo
+}
---
+++ js_aula01/script.js
@@ -0,0 +1,5 @@
+// Declaração de váriaveis JavaScript
+
+nome = prompt("Qual o seu nome?")
+idade = parseInt(prompt("Qual sua idade?"))
+curso = prompt("Qual seu curso?")
